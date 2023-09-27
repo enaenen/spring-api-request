ca63b01600fdad38bd3279050aba1c58b3ed72b7
Woo Joo Chae <enaenen@naver.com>
2023-09-27 16:00:39 +0900

    init

---
+++ src/main/java/com/example/springapirequest/SpringApiRequestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapirequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiRequestApplication.class, args);
+    }
+
+}
